ada0c2203c0c8db5eda23cf058ea0ece83461509
nazhanfahim <23002679@siswa.um.edu.my>
2024-11-12 12:22:22 +0800

    new code changes

---
+++ Viva1q5.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.viva1q5;
+import java.util.*;
+/**
+ *
+ * @author Dell
+ */
+public class Viva1q5 {
+
+    public static void main(String[] args) {
+    Scanner keyboard=new Scanner(System.in);
+    String in,out;
+    int count;
+        System.out.print("Please enter remix: ");
+        in=keyboard.nextLine();
+        
+            out=in.replaceAll("REMIX"," ");
+            
+            count=out.split("\\s").length;
+            
+            if(count!=0){
+                System.out.println("original: "+out);
+            }
+            else{
+                System.out.println("Please enter a remix with at least one word in it!");
+            }
+    }
+}
